cd187544e5ca58bfb33dc0216663718638f8bb24
Rich Higgins <rich.higgins@valtech.com>
2023-11-23 09:24:32 +0000

    added image for config (and build to succeed)

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gallery.png`, // This path is relative to the root of the site.
+        icon: `src/images/Gatsby_Monogram.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
